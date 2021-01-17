5b03822a15289a51ee8ba5c60ec2b7cc106d427c
Ozurumba <ozuruobinna@gmail.com>
2021-01-17 01:36:07 +0100

    initial commit

---
+++ src/app/configuration.ts
@@ -0,0 +1,9 @@
+export const configuration = {
+  header: {
+    heading: 'Chaka Trades',
+    headingtext:
+      'Chaka is a trading Company.',
+    buttontext: 'Start Trading',
+    buttonlink: '\home'
+  }
+};
---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+export interface Fruit {
+  name: string;
+}
+
+@Component({
+  selector: "app-gallery",
+  templateUrl: "./gallery.component.html",
+  styleUrls: ["./gallery.component.css"],
+})
+export class GalleryComponent implements OnInit {
+  // constructor() {}
+
+  lineChartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Stock prices'},
+  ];
+
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'lightgreen',
+      backgroundColor: 'white',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
+  ngOnInit() {}
+
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruits: Fruit[] = [
+    {name: 'Lemon'},
+    {name: 'Lime'},
+    {name: 'Apple'},
+  ];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+
+
+}
+
+
+
